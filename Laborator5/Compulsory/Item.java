ae986e57b20d20064deff2aa35011d422403250c
Alex Amarandei <alex.m.amarandei@gmail.com>
2021-03-23 21:58:14 +0200

    Completed Compulsory Tasks.

---
+++ Laborator5/Compulsory/Item.java
@@ -0,0 +1,38 @@
+/**
+ * @author Alex Amarandei
+ */
+
+package laborator5.compulsory;
+
+import java.io.Serializable;
+
+public abstract class Item implements Serializable {
+    private String name;
+    private String path;
+
+    public Item(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
