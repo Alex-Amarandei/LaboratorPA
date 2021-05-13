f858ab97c40a907b589ebc476a5e2d006c8e51b1
Alex Amarandei <alex.m.amarandei@gmail.com>
2021-05-13 13:10:56 +0300

    Completed Optional Tasks.

---
+++ Laborator10/Optional/Client/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+    public static final String address = "127.0.0.1";
+    public static final int PORT = 4444;
+
+    public static void main(String[] args) {
+        try(Socket socket = new Socket(address, PORT)) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                String request = scanner.nextLine();
+                out.println(request);
+                
+                if(request.equals("exit")) {
+                    System.out.println("Exiting client...");
+                    break;
+                }
+
+                String response = in.readLine();
+
+                System.out.println("Response received: " + response);
+                if(response.equalsIgnoreCase("Server stopped")) break;
+            }
+        } catch(IOException e) {
+            System.err.println("[Client Error] " + e.getMessage());
+        }
+    }
+}
